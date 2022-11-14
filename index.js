8b740a62bcaa43eea15b42bf983572bfeea311a6
AndreiMarin15 <misael_marin@dlsu.edu.ph>
2022-11-14 08:18:52 +0800

    setup crud and index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const hbc = require("hbs");
+const bodyparser = require("body-parser");
+
+const db = require("./models/db.js");
+
+const app = express();
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+dotenv.config();
+port = process.env.PORT;
+hostname = process.env.HOSTNAME;
+
+app.use(express.static(`public`));
+app.use(`/`, routes);
+
+db.connect();
+
+app.listen(port, hostname, function () {
+    console.log(`Server is running at:`);
+    console.log(`http://` + hostname + `:` + port);
+});
---
+++ models/db.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+
+const url = ""; // insert connection string to database
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+};
+
+const db = {
+    connect: () => {
+        mongoose.connect(url, options, (err) => {
+            if (err) throw err;
+            console.log("Connected to: " + url);
+        });
+    },
+
+    findOne: (model, query, projection, cb) => {
+        model.findOne(query, projection, (err, res) => {
+            if (err) return cb(false);
+            return cb(res);
+        });
+    },
+
+    findMany: (model, query, projection, cb) => {
+        model.find(query, projection, (err, res) => {
+            if (err) return cb(false);
+            return cb(res);
+        });
+    },
+
+    insertOne: (model, docu, cb) => {
+        model.create(docu, (err, res) => {
+            if (err) return cb(false);
+            console.log("Added: " + res);
+            return cb(true);
+        });
+    },
+
+    insertMany: (model, docs, cb) => {
+        model.insertMany(docs, (err, res) => {
+            if (err) return cb(false);
+            console.log("Added: " + res);
+            return cb(true);
+        });
+    },
+
+    updateOne: (model, filter, update, cb) => {
+        model.updateOne(filter, update, (err, res) => {
+            if (err) return cb(false);
+            console.log("Updated: " + res.nModified);
+            return cb(true);
+        });
+    },
+
+    updateMany: (model, filter, update, cb) => {
+        model.updateMany(filter, update, (err, res) => {
+            if (err) return cb(false);
+            console.log("Updated: " + res.nModified);
+            return cb(true);
+        });
+    },
+
+    delOne: (model, conditions, cb) => {
+        model.deleteOne(conditions, (err, res) => {
+            if (err) return cb(false);
+            console.log("Deleted: " + res.deletedCount);
+            return cb(true);
+        });
+    },
+
+    delMany: (model, conditions, cb) => {
+        model.deleteMany(conditions, (err, res) => {
+            if (err) return cb(false);
+            console.log("Deleted: " + res.deletedCount);
+            return cb(true);
+        });
+    },
+};
+
+module.exports = db;
